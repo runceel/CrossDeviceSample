b8ad3ec2d59053ce406b6b256ba3dc90026f0478
Kazuki Ota <kaota@microsoft.com>
2018-10-25 17:20:57 +0900

    impl

---
+++ CrossDeviceSample.WPF/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows;
+
+namespace CrossDeviceSample.WPF
+{
+    /// <summary>
+    /// App.xaml の相互作用ロジック
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            MainWindow = new MainWindow();
+            ((MainWindowViewModel)MainWindow.DataContext).ReceivedMessage = ParseArgs(e.Args);
+            MainWindow.Show();
+        }
+
+        private string ParseArgs(string[] args)
+        {
+            var first = args.FirstOrDefault();
+            if (string.IsNullOrEmpty(first))
+            {
+                return null;
+            }
+
+            var q = HttpUtility.ParseQueryString(new Uri(first).Query);
+            return q["message"];
+        }
+    }
+}
---
+++ CrossDeviceSample.WPF/MainWindowViewModel.cs
@@ -0,0 +1,72 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.AppService;
+
+namespace CrossDeviceSample.WPF
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private AppServiceConnection Connection { get; set; }
+        private string _status;
+        public string Status
+        {
+            get { return _status; }
+            set { SetProperty(ref _status, value); }
+        }
+
+        private string _receivedMessage;
+        public string ReceivedMessage
+        {
+            get { return _receivedMessage; }
+            set { SetProperty(ref _receivedMessage, value); }
+        }
+
+        public async Task InitializeConnectionAsync()
+        {
+            if (Connection != null)
+            {
+                return;
+            }
+
+            Connection = new AppServiceConnection
+            {
+                PackageFamilyName = Consts.CommunicationServiceHost,
+                AppServiceName = Consts.CommunicationServiceName,
+            };
+            Connection.RequestReceived += Connection_RequestReceived;
+            Connection.ServiceClosed += Connection_ServiceClosed;
+
+            while (true)
+            {
+                var status = await Connection.OpenAsync();
+                Status = $"{status}";
+                if (status == AppServiceConnectionStatus.Success)
+                {
+                    break;
+                }
+
+                await Task.Delay(2000);
+            }
+        }
+
+        private async void Connection_ServiceClosed(AppServiceConnection sender, AppServiceClosedEventArgs args)
+        {
+            Debug.WriteLine("Closed!!");
+            Connection = null;
+            await InitializeConnectionAsync();
+        }
+
+        private void Connection_RequestReceived(AppServiceConnection sender, AppServiceRequestReceivedEventArgs args)
+        {
+            if (args.Request.Message.TryGetValue("message", out var x))
+            {
+                ReceivedMessage = $"{x}";
+            }
+        }
+    }
+}
